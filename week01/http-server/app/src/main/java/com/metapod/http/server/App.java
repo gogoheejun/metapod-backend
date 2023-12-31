a45b5aa6785d7c0afc17c5095220c34e250ae388
gogoheejun <heejjuunn@gmail.com>
2023-12-31 13:57:22 +0900

    socket 개념 추가

--- week01/http-server/app/src/main/java/com/metapod/http/server/App.java
+++ week01/http-server/app/src/main/java/com/metapod/http/server/App.java
@@ -6,24 +6,28 @@ import java.net.Socket;
 import java.nio.CharBuffer;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         App app = new App();
         app.run();
     }
 
-    private void run() throws IOException {
+    private void run() throws IOException, InterruptedException {
         //1. Listen
         //서버소켓은 소켓과 똑같은거지만, 걍 accept이런 용도로 안쓰고 오직 리슨만 하는 용도로 쓰기 위해 구분한거임
         //new 하는 순간 listen알아서 됨.
-        ServerSocket listener = new ServerSocket(8080, 0); //backlog – requested maximum length of the queue of incoming connections. 0이면 자동
+        ServerSocket listener = new ServerSocket(8090, 0); //backlog – requested maximum length of the queue of incoming connections. 0이면 자동
 
         System.out.println("Listen!");
         //2. Accept
         while (true) {
+
             // curl localhost:8080을 하지 않으면 더이상 코드가 진행되지 않는데,
             // 이것을 Blocking이라고 함. -> 비동기, 멀티쓰레드가 필요한 이유임.
             Socket socket = listener.accept();
-            System.out.println("Accept!");
+            System.out.println("Accept!" + socket.getLocalPort());
+
+
+            Thread.sleep(10000);
 
             //3. 처리
             Reader reader = new InputStreamReader(socket.getInputStream());
